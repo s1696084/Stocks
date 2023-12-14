a8b5bcef8afed7e5b512ae95afbb7136a6f691db
Vivian R <131028421+s1696084@users.noreply.github.com>
2023-12-14 14:28:24 -0600

    Update Stock.py

--- Stock.py
+++ Stock.py
@@ -1,7 +1,5 @@
 import requests
-from alpaca.trading.client import TradingClient
-from alpaca-trade-api alpaca-py import MarketOrderRequest
-from alpaca.trading.enums import OrderSide, TimeInForce
+import alpaca_trade_api as tradeapi
 
 alpaca_api_key = 'PKBL3JHF9AQTJPEVXPB5'
 alpaca_secret_key = 'lnYLaAvAp34Pr2BS6YRjf9sonIltjuVFuf2vIs3f'
@@ -10,9 +8,10 @@ base_url = "https://paper-api.alpaca.markets"
 weather_api_key = "bdb08a1cfe0e641014d62d0fca4d82a8"
 weather_api_url = "http://api.openweathermap.org/data/2.5/weather"
 
-stock_symbol = "APPL"
+stock_symbol = "AAPL"  # Assuming you meant "AAPL" instead of "APPL"
 
-trading_client = TradingClient(alpaca_api_key, alpaca_secret_key, paper=True)
+# Initialize the Alpaca API client
+alpaca_client = tradeapi.REST(alpaca_api_key, alpaca_secret_key, base_url=base_url, api_version='v2')
 
 def get_current_weather():
     response = requests.get(f"{weather_api_url}?q=your_location&appid={weather_api_key}")
@@ -42,7 +41,7 @@ def get_last_game_result(team_id):
         return f"No results found for team ID {team_id}. Response: {data}"
 
 def buy_stock():
-    account = trading_client.get_account()
+    account = alpaca_client.get_account()
 
     team_id_houston_rockets = 10
     result = get_last_game_result(team_id_houston_rockets)
@@ -53,19 +52,14 @@ def buy_stock():
     if get_current_weather() and result == 'Win':
         if float(account.buying_power) > 0:
             # Preparing orders
-            market_order_data = MarketOrderRequest(
+            alpaca_client.submit_order(
                 symbol=stock_symbol,
                 qty=1,
-                side=OrderSide.BUY,
-                time_in_force=TimeInForce.DAY
+                side='buy',
+                type='market',
+                time_in_force='gtc'
             )
-
-            # Submitting the market order
-            try:
-                market_order = trading_client.submit_order(order_data=market_order_data)
-                print(f"Market order placed to buy {stock_symbol}. Order ID: {market_order.id}")
-            except Exception as e:
-                print(f"Error placing order: {e}")
+            print(f"Market order placed to buy {stock_symbol}.")
         else:
             print("Not enough buying power to place the order.")
     else:
