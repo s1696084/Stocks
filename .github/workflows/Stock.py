c24821a0b26d8917d5c9ed0f6848607d08817b0c
Vivian R <131028421+s1696084@users.noreply.github.com>
2023-12-14 15:55:18 -0600

    Update Stock.py

--- .github/workflows/Stock.py
+++ .github/workflows/Stock.py
@@ -3,8 +3,8 @@ from alpaca.trading.client import TradingClient
 from alpaca.trading.requests import MarketOrderRequest
 from alpaca.trading.enums import OrderSide, TimeInForce
 
-alpaca_api_key = 'PKBL3JHF9AQTJPEVXPB5'
-alpaca_secret_key = 'lnYLaAvAp34Pr2BS6YRjf9sonIltjuVFuf2vIs3f'
+alpaca_api_key = 'PKL9JQMNWF0FS4Z6X7I8'
+alpaca_secret_key = 'qjJs7YcUZ9IcQa1siHzM8eZpFcTJ7vLbpaLOIr37'
 base_url = "https://paper-api.alpaca.markets"
 
 weather_api_key = "bdb08a1cfe0e641014d62d0fca4d82a8"
